8c19553202b0c4caee5931d3cf0b46b930759a07
LaureAnneMary <laureanne.marechal@viacesi.fr>
2016-11-04 09:38:11 +0100

    Compute addition of two integers

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+int addition (int a, int b){
+    return a+b;
+}
+
+
+
+
 
 int main()
 {
-    printf("Hello world!\n");
+    int a, b;
+    printf("Choisir deux chiffres :");
+    scanf("%d", &a);
+    printf( " et ");
+    scanf("%d", &b);
+    printf("Et le resultat est ... %d", addition( a, b));
     return 0;
 }
